66327140dfc8f59d946ee8bdd53241ade5620885
lemax97 <lemax97@mail.ru>
2020-10-31 00:17:50 +0300

    Initial commit

---
+++ lwjgl3/src/main/java/SPC/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package SPC.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import SPC.Core;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new Core(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("SpaceCowboys");
+		configuration.setWindowedMode(800, 600);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
